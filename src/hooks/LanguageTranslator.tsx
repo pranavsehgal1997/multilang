4b00e94af78d2b35a4f3ac6cbde714d7d213cbe9
Pranav Sehgal <pranav.sehgal@in.ey.com>
2024-07-30 13:18:55 +0530

    updated the comments

--- src/hooks/LanguageTranslator.tsx
+++ src/hooks/LanguageTranslator.tsx
@@ -1,19 +1,33 @@
 import { createContext, useContext } from "react";
 
+// Define a type for the language translator context
 type LanguageTranslator = {
-  language: { [key: string]: string };
-  setLanguage: React.Dispatch<React.SetStateAction<string>>;
+  language: { [key: string]: string }; // Object representing the language translations
+  setLanguage: React.Dispatch<React.SetStateAction<string>>; // Function to update the language
 };
 
+// Create a context with the type LanguageTranslator, initialized to undefined
+/**
+ * LanguageTranslator: Context to store the language translations and the function to update the language
+   * @param language: Object representing the language translations (key:value pair)
+   * @param setLanguage: Function to update the language (string value)
+   
+   */
 const LanguageTranslator = createContext<LanguageTranslator | undefined>(
   undefined
 );
 
+// Custom hook to use the LanguageTranslator context
 const useLanguageTranslator = (): LanguageTranslator => {
+  // Get the context value
   const context = useContext(LanguageTranslator);
+  // If the context is undefined, throw an error
   if (context === undefined) {
-    throw new Error("");
+    throw new Error(
+      "useLanguageTranslator must be used within a LanguageTranslatorProvider"
+    );
   }
+  // Return the context value
   return context;
 };
 
